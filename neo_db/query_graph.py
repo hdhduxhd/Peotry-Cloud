29222fba7f044eeec9c17485d3cbfbde54db1f6b
hzhy <1425914241@qq.com>
2023-03-21 15:12:50 +0800

    init

---
+++ neo_db/query_graph.py
@@ -0,0 +1,76 @@
+from neo_db.config import graph, CA_LIST, similar_words
+from spider.show_profile import get_profile
+import codecs
+import os
+import json
+import base64
+
+def query(name):
+    data = graph.run(
+    "match(p:诗人) -[r]->(n:`诗歌`) where p.name='%s' return p.name,n.name,r.relation" % (name)
+    )
+    data = list(data)
+    return get_json_data(data)
+def get_json_data(data):
+    json_data={'data':[],"links":[]}
+    d=[]
+    
+    
+    for i in data:
+        # print(i["p.Name"], i["r.relation"], i["n.Name"], i["p.cate"], i["n.cate"])
+        d.append(i['p.name'])
+        d.append(i['n.name'])
+        d=list(set(d))
+    name_dict={}
+    count=0
+    for j in d:
+        data_item={}
+        name_dict[j]=count
+        count+=1
+        data_item['name']=j
+        json_data['data'].append(data_item)
+    for i in data:
+   
+        link_item = {}
+        
+        link_item['source'] = name_dict[i['p.name']]
+        
+        link_item['target'] = name_dict[i['n.name']]
+        link_item['value'] = i['r.relation']
+        json_data['links'].append(link_item)
+
+    return json_data
+# f = codecs.open('./static/test_data.json','w','utf-8')
+# f.write(json.dumps(json_data,  ensure_ascii=False))
+def get_KGQA_answer(array):
+    data_array=[]
+    for i in range(len(array)-2):
+        if i==0:
+            name=array[0]
+        else:
+            name=data_array[-1]['p.Name']
+           
+        data = graph.run(
+            "match(p)-[r:%s{relation: '%s'}]->(n:Person{Name:'%s'}) return  p.Name,n.Name,r.relation,p.cate,n.cate" % (
+                similar_words[array[i+1]], similar_words[array[i+1]], name)
+        )
+       
+        data = list(data)
+        print(data)
+        data_array.extend(data)
+        
+        print("==="*36)
+    with open("./spider/images/"+"%s.jpg" % (str(data_array[-1]['p.Name'])), "rb") as image:
+            base64_data = base64.b64encode(image.read())
+            b=str(base64_data)
+          
+    return [get_json_data(data_array), get_profile(str(data_array[-1]['p.Name'])), b.split("'")[1]]
+def get_answer_profile(name):
+    with open("./spider/images/"+"%s.jpg" % (str(name)), "rb") as image:
+        base64_data = base64.b64encode(image.read())
+        b = str(base64_data)
+    return [get_profile(str(name)), b.split("'")[1]]
+        
+
+
+
---
+++ test.py
@@ -0,0 +1,127 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+def ResNet18():
+    return ResNet(BasicBlock, [2,2,2,2])
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+batch_size = 64
+
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('data', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=batch_size, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('data', train=False, transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=batch_size, shuffle=True)
+
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # 将一批的损失相加
+            pred = output.argmax(dim=1, keepdim=True)  # 找到概率最大的下标
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = ResNet18().to(device)
+optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+
+epochs = 10
+for epoch in range(1, epochs + 1):
+    train(model, device, train_loader, optimizer, epoch)
+    test(model, device, test_loader)
