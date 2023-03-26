d4ea755c46b57ca3e6fd624c91a8db8c0ef01ab1
hzhy <1425914241@qq.com>
2023-03-26 19:43:57 +0800

    push test

--- neo_db/query_graph.py
+++ neo_db/query_graph.py
@@ -7,7 +7,7 @@ import base64
 
 def query(name):
     data = graph.run(
-    "match(p:诗人) -[r]->(n:`诗歌`) where p.name='%s' return p.name,n.name,r.relation" % (name)
+    "match(p:诗人) -[r]->(n:`诗歌`) where p.name='%s' return p.name,n.name,r.relation limit 50" % (name)
     )
     data = list(data)
     return get_json_data(data)
@@ -48,10 +48,10 @@ def get_KGQA_answer(array):
         if i==0:
             name=array[0]
         else:
-            name=data_array[-1]['p.Name']
+            name=data_array[-1]['p.name']
            
         data = graph.run(
-            "match(p)-[r:%s{relation: '%s'}]->(n:Person{Name:'%s'}) return  p.Name,n.Name,r.relation,p.cate,n.cate" % (
+            "match(p)-[r:%s{relation: '%s'}]->(n:Person{Name:'%s'}) return  p.name,n.name,r.relation" % (
                 similar_words[array[i+1]], similar_words[array[i+1]], name)
         )
        
@@ -60,7 +60,7 @@ def get_KGQA_answer(array):
         data_array.extend(data)
         
         print("==="*36)
-    with open("./spider/images/"+"%s.jpg" % (str(data_array[-1]['p.Name'])), "rb") as image:
+    with open("./spider/images/"+"%s.jpg" % (str(data_array[-1]['p.name'])), "rb") as image:
             base64_data = base64.b64encode(image.read())
             b=str(base64_data)
           
